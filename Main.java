d6b2b880bc2eef1b35ec84a6f05e433198510a55
juanjaviermino <109086949+juanjaviermino@users.noreply.github.com>
2025-01-08 01:59:42 +0000

    Corregir error en saludo de nuevo

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
+        System.out.println("Versión 1.0.0 lista para producción.");
     }
-
-    System.out.println("Versión 1.0.0 lista para producción.");
 }
