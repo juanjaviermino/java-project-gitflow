4ad060377c0dd96e5d55acd8006567124744edd3
juanjaviermino <109086949+juanjaviermino@users.noreply.github.com>
2025-01-14 18:45:51 +0000

    Añadir logotipo ASCII en la Main, antes del saludo

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ public class Main {
         Saludo saludo = new Saludo();
         Despedida despedida = new Despedida();
 
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         System.out.println(saludo.generarSaludo("Mundo"));
         System.out.println(despedida.generarDespedida("Mundo"));
         System.out.println("Versión 1.0.0 lista para producción.");
